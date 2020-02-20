d3a6b2516e55e13f9ec95b2ffd8d7e864657ec4f
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 14:43:38 +0530

    Initial commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double c;
+            Console.WriteLine("enter a");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter b");
+            b = int.Parse(Console.ReadLine());
+            c = multiply(a, b);
+            add(a, b);
+
+            Console.WriteLine("product of {0} and {1}={2}", a, b, c);
+        }
+        public static double multiply(int a, int b)
+        {
+            double prod = a * b;
+            return prod;
+        }
+         
+        static void add(int a,int b)
+        {
+            int d = a + b;
+            Console.WriteLine("addition of two numbers" + d);
+        }
+
+      
+        
+    }
+}
